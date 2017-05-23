28a6fb731746a9e5b2bb884d871adcab1adf0959
YasirYaqoob <yasiryasir38@gmail.com>
2017-05-22 22:16:35 -0700

    yasir

---
+++ app/src/main/java/com/example/yasir/myapplication/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.yasir.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText t;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //commet
+        t= (EditText) findViewById(R.id.text);
+    }
+}
